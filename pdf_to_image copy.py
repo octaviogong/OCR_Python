e28f8c6890e0df81a8c2b2fa4e8b3e403bf95033
octaviogong <oky9401@gmail.com>
2023-02-25 17:38:21 -0600

    update

---
+++ pdf_to_image copy.py
@@ -0,0 +1,44 @@
+from pdf2image import convert_from_path
+import os
+from PIL import Image
+import pytesseract
+import cv2 as cv
+
+
+#PDF a imagen JPEG
+poppler_path = r"C:\Users\Octavio\Downloads\poppler-0.68.0\poppler-0.68.0\bin"
+pdf_path = r"C:\OCR_Python\HC JARAL\HC JARAL\A0207.pdf"
+
+
+pages = convert_from_path(pdf_path = pdf_path, poppler_path = poppler_path)
+saving_folder = r"C:\OCR_Python"
+c = 1
+for page in pages:
+    img_name = f"img{c}.jpeg"
+    page.save(os.path.join(saving_folder,img_name),"JPEG")
+    c+=1
+
+
+#Mejora de la nitidez de imagen
+
+img_cv = cv.imread(img_name)
+img_cv = cv.cvtColor(img_cv, cv.COLOR_RGB2GRAY)
+
+adaptative = cv.adaptiveThreshold(img_cv,255, cv.ADAPTIVE_THRESH_GAUSSIAN_C,cv.THRESH_BINARY,201,5)
+
+cv.imwrite("Adapt.jpeg",adaptative)
+
+#Extracción del texto
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+texto = pytesseract.image_to_string(adaptative)
+
+#Archivo txt de salida
+salida_txt = open('img1.txt','w')
+salida_txt.write(texto)
+salida_txt.close()
+
+
+
+
+
+
